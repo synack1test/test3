042f0397f42fbe2772d35943f952d6be9d8dac03
synack1test <43527438+synack1test@users.noreply.github.com>
2021-12-23 01:28:25 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def_main():
+	DSA_PRIVATE_KEY = 'asdad'
+	EC_PRIVATE_KEY = 'asdad'
+	OPENSSH_PRIVATE_KEY = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+	PGP_PRIVATE_KEY_BLOCK= 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+	PRIVATE_KEY= 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+	RSA_PRIVATE_KEY= 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+	SSH2_ENCRYPTED_PRIVATE_KEY= 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+	PuTTY-User-Key-File-2= 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+main()
